d44398a7ada9021d456f2523fcab9b6c91228177
renup <rpunjabi12@gmail.com>
2023-05-04 05:35:56 -0700

    initial commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by renupunjabi on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
